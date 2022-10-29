9a78ffdabb459030febfae8c911d06519564c36d
Lidkovsky <armkudr@gmail.com>
2022-10-29 21:42:53 +0500

    CTA button added and radar reAnimated

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './styles/App.css';
 import Radar from "./components/radar/Radar";
 import ServiceCard from './components/service card/Service-Card';
+import StatsInfo from './components/stats info/stats-info';
+import RtwBtn from './components/ready to work btn/rtw-btn';
 
 function App() {
     return (
         <div className="App" style={{display: 'flex', flexDirection: 'column'}}>
             <Radar />
             <ServiceCard title="Telegram" description={["Таргетная реклама", "Создание постов", "Интеграции"]}/>
+            <StatsInfo />
+            
+            <RtwBtn />
+            
         </div>
     );
 }
--- src/components/radar/Radar.tsx
+++ src/components/radar/Radar.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { motion , useAnimation} from "framer-motion"
 import './Radar.css';
-import chain from '../../images/chain.png';
-import connection from '../../images/connection.png';
-import facebook from '../../images/facebook.png';
-import graph from '../../images/graph.png';
-import instagram from '../../images/instagram.png';
-import messenger from '../../images/messenger.png';
-import radar from '../../images/radar.png';
-import social from '../../images/social.png';
-import sound from '../../images/sound.png';
-import stat from '../../images/stat.png';
-import telegram from '../../images/telegram.png';
+import chain from '../../images/chain.svg';
+import connection from '../../images/connection.svg';
+import facebook from '../../images/facebook.svg';
+import graph from '../../images/graph.svg';
+import instagram from '../../images/instagram.svg';
+import messenger from '../../images/messenger.svg';
+import radar from '../../images/radar.svg';
+import social from '../../images/social.svg';
+import sound from '../../images/sound.svg';
+import stat from '../../images/stat.svg';
+import telegram from '../../images/telegram.svg';
 
 const Radar = () => {
+
+    const iconsArr = [chain, facebook, graph, instagram, messenger, radar, social, stat, telegram];
+
     return (
         <div id="crop">
             <div id="radar-container">
@@ -21,6 +25,19 @@ const Radar = () => {
                     <h1 className="H1">Digital Marketing Agency</h1>
                 </div>
                 <div id="background">
+                    {
+                iconsArr.map((item)=>{
+                    return(<motion.img
+                    animate={{x: Math.floor(Math.random() * (1000))-500 , y: Math.floor(Math.random() * (-250)-70)}} 
+                    transition={{
+                        ease:"linear",
+                        duration: 2.5,
+                     repeat: Infinity, 
+                     repeatDelay: 2.5
+                    }}
+                    src={item} alt="" id={item}/>)
+                })
+            }
                     <img src={chain} alt="" id="chain"/>
                     <img src={connection} alt="" id="connection"/>
                     <img src={facebook} alt="" id="facebook"/>
@@ -34,8 +51,10 @@ const Radar = () => {
                     <img src={telegram} alt="" id="telegram"/>
                 </div>
                 <div id="radar-parent">
-                    <div id="radar">
-                    </div>
+                    <motion.div id="radar"
+                    animate={{rotate:-360}}
+                    transition={{ ease:'linear',repeat: Infinity, duration: 5}}>
+                    </motion.div>
                 </div>
             </div>
         </div>
