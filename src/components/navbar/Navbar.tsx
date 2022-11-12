66d934957690f147fdebe1e0847dc81c194796b3
Matvei <matvesch@protonmail.com>
2022-11-12 16:33:26 +0200

    added burger button and options to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../images/logo.png'
-import NavbarButton from '../../ui/navbar-button/NavbarButton'
-import { Link } from 'react-router-dom'
+import { useState, useRef } from 'react';
+import './Navbar.css';
+import logo from '../../images/logo.png';
+import NavbarButton from '../../ui/navbar-button/NavbarButton';
+import { Link } from 'react-router-dom';
+import burger from '../../images/burger.svg';
 
 const Navbar = () => {
+    const [showOptions, setShowOptions] = useState(false);
+    const wrapperRef = useRef<HTMLImageElement>(null);
+
+    function handleClickOutside(e: { target: any }) {
+        if (wrapperRef.current && !wrapperRef.current.contains(e.target)) {
+            setShowOptions(false);
+        }
+    }
+
+    function handleOptionClick() {
+        setShowOptions(false);
+    }
+
     return (
-        <nav id="navbar">
+        <nav id="navbar" onClick={handleClickOutside}>
             <Link to="/">
                 <div id="logo">
                     <img src={logo} alt="logo" />
@@ -21,8 +35,42 @@ const Navbar = () => {
                     <NavbarButton buttonName={'Contact'} linkTo={'/contact'} />
                 </div>
             </div>
+            <img
+                src={burger}
+                ref={wrapperRef}
+                alt="burger-icon"
+                className="burger-icon"
+                onClick={() => setShowOptions(!showOptions)}
+            />
+            <div
+                className="menu-options"
+                style={showOptions ? { display: 'block' } : { display: 'none' }}
+            >
+                <NavbarButton
+                    buttonName={'Services'}
+                    linkTo={'/services'}
+                    onClick={handleOptionClick}
+                />
+                <NavbarButton
+                    buttonName={'About us'}
+                    linkTo={'/'}
+                    onClick={handleOptionClick}
+                />
+                <NavbarButton
+                    buttonName={'Research'}
+                    linkTo={'/'}
+                    onClick={handleOptionClick}
+                />
+                <div id="contact-btn">
+                    <NavbarButton
+                        buttonName={'Contact'}
+                        linkTo={'/contact'}
+                        onClick={handleOptionClick}
+                    />
+                </div>
+            </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
