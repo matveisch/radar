465d8d3ab5f975f97669224d3e300339123e5a31
Matvei <matvesch@protonmail.com>
2022-11-17 09:00:19 +0200

    new carousel page, component and block ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './styles/App.css'
-import RtwBtn from './components/ready to work btn/RtwBtn'
-import Technology from './pages/technology/technology'
-import Services from './pages/services section/ServicesSection'
-import RadarPage from './pages/radar-page/RadarPage'
+import './styles/App.css';
+import RtwBtn from './components/ready to work btn/RtwBtn';
+import Technology from './pages/technology/technology';
+import Services from './pages/services section/ServicesSection';
+import RadarPage from './pages/radar-page/RadarPage';
+import CarouselPage from './pages/carousel-page/CarouselPage';
 
 function App() {
     return (
@@ -13,9 +13,13 @@ function App() {
                 style={{ display: 'flex', flexDirection: 'column' }}
             >
                 <RadarPage />
+                <Services />
+                <Technology />
+                <CarouselPage />
+                <RtwBtn />
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
